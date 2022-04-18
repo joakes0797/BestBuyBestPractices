2ff87dd22119802698d43e474aa558c6b47e6f4b
Jill Oakes <jericho_rules@hotmail.com>
2022-04-18 16:16:01 -0400

    Change StockLevel to string and Update IProdRepo

--- IProductRepository.cs
+++ IProductRepository.cs
@@ -8,5 +8,7 @@ namespace BestBuyBestPractices
     {
         public IEnumerable<Product> GetAllProducts();
         public void CreateProduct(string name, double price, int categoryID);
+        public void UpdateProduct(int productID, string newName);
+        public void DeleteProduct(int productID);
     }
 }
--- Product.cs
+++ Product.cs
@@ -16,6 +16,6 @@ namespace BestBuyBestPractices
         public double Price { get; set; }
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
-        public int StockLevel { get; set; }
+        public string StockLevel { get; set; }
     }
 }
